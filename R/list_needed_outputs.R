98f8cfd8e36999673b959ff5a89e7fec95aeff64
stenzel <stenzel@pik-potsdam.de>
2023-03-07 17:28:28 +0100

    updated list_needed_outputs

--- R/list_needed_outputs.R
+++ R/list_needed_outputs.R
@@ -8,8 +8,8 @@
 #' and/or planetary boundary calculations
 #'
 #' @param metric string/list of strings, containing name of metric to get
-#'        required outputs for. Pick from "meco", "mcol", "biome", "all_pbs",
-#'               "pb_n", "pb_w", "pb_b", "pb_lsc", "all" (default)
+#'        required outputs for. Pick from "meco", "mcol", "biome", "nitrogen",
+#'      "lsc", "bluewater", "greenwater", "water", "biosphere", "all" (default)
 #'
 #' @param withNitrogen logical: include nitrogen outputs? default: TRUE
 #'
@@ -20,71 +20,105 @@
 #'
 #' }
 #' @export
-list_needed_outputs <- function(metric = "all", with_nitrogen = TRUE) {
+list_needed_outputs <- function(metric = "all", with_nitrogen = TRUE,
+                                only_first_filename = TRUE) {
 
-  optional_metrics <- c("meco", "mcol", "biome", "all_pbs", "pb_n",
-                        "pb_w", "pb_b", "pb_lsc", "all")
+  optional_metrics <- c("meco", "mcol", "biome", "nitrogen", "lsc", "benchmark",
+                        "bluewater", "greenwater", "water", "biosphere", "all")
   notin <- metric[!metric %in% optional_metrics]
   if (length(notin) > 0) {
     stop(paste("Metrics not available:", notin, collapse = ", "))
   }
-  varsGAMMA <- c("grid", "fpc", "fpc_bft", "cftfrac", "firec", "rh_harvest",
-                 "npp", "runoff", "transp", "vegc", "firef", "rh", "harvestc",
-                 "evap", "interc", "soilc", "litc", "swc")
-  varsGAMMAnitrogen <- c("vegn", "soilnh4", "soilno3", "leaching", "n2o_denit",
-                         "n2o_nit", "n2o_denit", "n2_emis", "bnf",
-                         "n_volatilization")
-  varsHANPP <- c("grid", "npp", "pft_npp", "pft_harvestc", "pft_rharvestc",
-                 "firec", "timber_harvestc", "cftfrac", "fpc")
-  varsBiome <- c("grid", "fpc", "vegc", "pft_lai", "temp")
-  vars_pb_n <- c("grid", "runoff", "leaching", "pet", "prec")
-  vars_pb_w <- c("grid", "discharge", "irrig", "drainage")
-  vars_pb_lsc <- varsBiome
-  vars_pb_b <- c()
+
+  requirements <- system.file("extdata",
+                              "metric_files.yaml",
+                              package = "boundaries") %>%
+    yaml::read_yaml()
 
   outs <- c()
-  tsteps <- c() # todo: add timestep functionality - needs to be clever
-
-  if ("all" %in% metric) {
-    if (with_nitrogen) {
-      outs <- c(outs,varsGAMMA, varsGAMMAnitrogen, varsHANPP, varsBiome,
-                           vars_pb_n, vars_pb_w, vars_pb_lsc, vars_pb_b)
-    } else {
-      outs <- c(outs,varsGAMMA, varsHANPP, varsBiome, vars_pb_w, vars_pb_lsc,
-                vars_pb_b)
-    }
-  }
   if ("meco" %in% metric) {
     if (with_nitrogen) {
-      outs <- c(outs,varsGAMMA, varsGAMMAnitrogen)
+      outs <- c(outs, requirements[["meco"]], requirements[["meco_nitrogen"]])
     } else {
-      outs <- c(outs,varsGAMMA)
+      outs <- c(outs, requirements[["meco"]])
     }
-  }
-  if ("biome" %in% metric) {
-    outs <- c(outs,varsBiome)
   }
   if ("mcol" %in% metric) {
-    outs <- c(outs,varsHANPP)
+    outs <- c(outs, requirements[["mcol"]])
   }
-  if ("all_pbs" %in% metric) {
+  if ("biome" %in% metric) {
+    outs <- c(outs,requirements[["biome"]])
+  }
+  if ("nitrogen" %in% metric) {
+    if (!with_nitrogen)
+      stop("You requested the nitrogen boundary without nitrogen?! Aborting.")
+    outs <- c(outs, requirements[["nitrogen"]])
+  }
+  if ("lsc" %in% metric) {
+    outs <- c(outs, requirements[["lsc"]])
+  }
+  if ("bluewater" %in% metric) {
+    outs <- c(outs, requirements[["bluewater"]])
+  }
+  if ("greenwater" %in% metric) {
+    outs <- c(outs, requirements[["greenwater"]])
+  }
+  if ("water" %in% metric) {
+    outs <- c(outs, requirements[["bluewater"]], requirements[["greenwater"]])
+  }
+  if ("biosphere" %in% metric) {
+    outs <- c(outs, requirements[["mcol"]], requirements[["meco"]])
+  }
+  if ("all" %in% metric) {
     if (with_nitrogen) {
-      outs <- c(outs,varsBiome, vars_pb_n, vars_pb_w, vars_pb_lsc, vars_pb_b)
+      outs <- c(outs, requirements[["meco"]], requirements[["mcol"]],
+                requirements[["meco_nitrogen"]], requirements[["nitrogen"]],
+                requirements[["biome"]], requirements[["bluewater"]],
+                requirements[["greenwater"]], requirements[["lsc"]])
     } else {
-      outs <- c(outs,varsBiome, vars_pb_w, vars_pb_lsc, vars_pb_b)
+      outs <- c(outs, requirements[["meco"]], requirements[["mcol"]],
+                requirements[["biome"]], requirements[["bluewater"]],
+                requirements[["greenwater"]], requirements[["lsc"]])
     }
   }
-  if ("pb_n" %in% metric) {
-    outs <- c(outs,vars_pb_n)
+  if ("benchmark" %in% metric){
+    outs <- c(requirements[["benchmark"]])
   }
-  if ("pb_w" %in% metric) {
-    outs <- c(outs,vars_pb_w)
+  out <- unify_list(outs)
+
+  if (only_first_filename){
+    output_filenames <- lapply(out,function(x) x[["file_name"]][1])
+  }else{
+    output_filenames <- lapply(out,function(x) x[["file_name"]])
   }
-  if ("pb_lsc" %in% metric) {
-    outs <- c(outs,vars_pb_lsc)
+  return(list(outputs = output_filenames,
+              timesteps = sapply(out,function(x) x[["resolution"]])))
+}
+
+# for input list a, all duplicate keys are unified, taking the value with
+#     highest temporal resolution (daily>monthly>annual)
+unify_list <- function(a) {
+  merged_list <- list()
+  # Iterate over all unique keys
+  for (item in unique(names(a))){
+    indices <- which(names(a) == item)
+    # Get the values for the current key
+    values <- sapply(a[indices], function(x) x[["resolution"]])
+    outval <- a[indices[1]]
+    outval[[item]]$resolution <- highest_temporal_res(values)
+    merged_list <- c(merged_list, outval)
   }
-  if ("pb_b" %in% metric ) {
-    outs <- c(outs,vars_pb_b)
+  return(merged_list)
+}
+
+# Among list of input values a, return that with highest temporal resolution
+# (daily>monthly>annual)
+highest_temporal_res <- function(a) {
+  if ("daily" %in% a) {
+    return("daily")
+  } else if ("monthly" %in% a) {
+    return("monthly")
+  } else {
+    return("annual")
   }
-  return(list(outputs = unique(sort(outs)), timesteps = tsteps))
 }
